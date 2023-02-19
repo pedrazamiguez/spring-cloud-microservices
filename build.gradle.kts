de9aa03e9f66a0dd86d70b976fc28decc7988184
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2023-02-19 08:54:25 +0100

    Configure gradle for subprojects.

---
+++ build.gradle.kts
@@ -0,0 +1,97 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    id("org.springframework.boot") version("3.0.2") apply(false)
+    id("io.spring.dependency-management") version("1.1.0")
+    kotlin("jvm") version("1.7.22")
+    kotlin("plugin.spring") version("1.7.22")
+}
+
+subprojects {
+
+    apply {
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+    }
+
+    group = "eu.pedrazamiguez.microservices"
+    version = "0.0.1-SNAPSHOT"
+    java.sourceCompatibility = JavaVersion.VERSION_17
+
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://artifactory-oss.prod.netflix.net/artifactory/maven-oss-candidates")
+        }
+    }
+
+    extra["springCloudVersion"] = "2022.0.1"
+
+    val implementation by configurations
+    val developmentOnly by configurations
+    val testImplementation by configurations
+
+    dependencies {
+
+        implementation(kotlin("stdlib-jdk8"))
+        implementation(kotlin("reflect"))
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+        implementation("org.springframework.boot:spring-boot-starter-web")
+
+        implementation("org.springframework.cloud:spring-cloud-starter-config")
+        implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+        implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+        implementation("org.springframework.boot:spring-boot-starter-aop")
+        implementation("io.github.resilience4j:resilience4j-spring-boot3")
+
+        implementation("io.micrometer:micrometer-observation")
+        implementation("io.micrometer:micrometer-tracing-bridge-brave")
+        implementation("io.github.openfeign:feign-micrometer")
+        implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+        implementation("org.springframework.amqp:spring-rabbit")
+
+        developmentOnly("org.springframework.boot:spring-boot-devtools")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.micrometer:micrometer-observation-test")
+
+    }
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<BootBuildImage> {
+        imageName.set("apedraza/${project.name}:latest")
+        publish.set(false)
+        docker {
+            publishRegistry {
+                username.set("apedraza")
+                password.set("dckr_pat_2bEUK9eTdaSzpxNNqVXyGgrKXyc")
+            }
+        }
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "spring-cloud-microservices"
+include(
+        "api-gateway",
+        "server-configuration",
+        "server-discovery",
+        "service-file",
+        "service-folder",
+        "service-navigation",
+        "service-search",
+        "service-authentication",
+)
